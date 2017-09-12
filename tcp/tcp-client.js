ed762168ef8516bb18571be8eceea17567495d72
Pabllo Felipe de Andrade <cc147955@inf.ufg.br>
2017-09-11 22:33:45 -0300

    Update tcp-client.js

--- tcp/tcp-client.js
+++ tcp/tcp-client.js
@@ -1,5 +1,7 @@
 'use strict';
-
+/*
+ Declaração de variáveis
+*/
 let
     net = require('net'),
     readline = require('readline'),
@@ -8,28 +10,52 @@ let
     client = null,
     myNick = null,
     portCliente = null,
-		conectado = null;
+	conectado = null;
+/**
+Função connect()
+Abre conexão com servidor via tcp
+@port porta do servidor
+@host ip do servidor
+*/
+client = net.connect({ port: port, host: host });
 
-client = net.connect({ port: port, host: host }, function () {
-		//client.write(myNick + '**' + port);
-});
+/**
+Função on()
+Exibe dados ecoados do servidor
+*/
 client.on('data', function (data) {
 	data = data.toString();
 	console.log(data);
 });
+/**
+Função on()
+Exibe que a conexão foi fechada
+*/
 client.on('end', function () {
 		console.log('Server has disconnected.');
 		process.exit();
 });
 
 
-
+/**
+Função getNick()
+@return o nickname
+*/
 function getNick(){
   console.log("What's your name?");
 }
+/**
+Função getPort()
+@return a porta
+*/
 function getPort(){
   console.log("What's your port?");
 }
+
+/**
+Função createInterface()
+recebe dados digitados no console
+*/
 readline.createInterface({
     input: process.stdin,
     output: process.stdout
